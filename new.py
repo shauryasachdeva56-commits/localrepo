3e24fc04e04e3c6ab812373b90ed78342019008c
shauryasachdeva56-commits <shauryasachdeva56@gmail.com>
2025-10-29 14:21:14 +0530

    new addiction

---
+++ new.py
@@ -0,0 +1,6 @@
+list= [shaurya, devesh, ankit, rahul
+       for name in list:
+        if len(name)>5:
+            print(f"{name} has more than 5 letters")
+        else:
+            print(f"{name} has 5 or fewer letters")]
